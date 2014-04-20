55f0fbd005f7902f2466456a5351eebfe65dd7a8
Dhruv Goel <dhruvgoel2295@gmail.com>
2014-04-21 01:25:10 +0530

    Initialize repository

---
+++ config/boot.rb
@@ -0,0 +1,7 @@
+# Set up gems listed in the Gemfile.
+
+ENV['EXECJS_RUNTIME'] = 'Node'
+
+ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
+
+require 'bundler/setup' if File.exist?(ENV['BUNDLE_GEMFILE'])
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+FirstApp::Application.config.secret_key_base = '37d7e93c0380ffb5485e4dfb51795c00b6f196760704f1d0ecc6edc97a9d5b40cf45b8899caa8b59354c8c0a978f687a959524dbc73a7e316d5b073a53cd5246'
